ee1e723938123a985d7312a1391b3879e948b34c
Bernhard Straub <bsoft@mandel-straub.de>
2025-10-07 09:27:42 +0200

    Remove old project

--- PartialClassTest/Components/Counter2Display.razor.cs
+++
@@ -1,8 +0,0 @@
-using RxBlazorV2Sample.Model;
-using RxBlazorV2.Component;
-
-namespace RxBlazorV2Sample.Components;
-
-public partial class Counter2Display : ObservableComponent<CounterModel>
-{
-}
--- PartialClassTest/Pages/Counter1.razor.cs
+++
@@ -1,9 +0,0 @@
-using RxBlazorV2.Component;
-using RxBlazorV2Sample.Model;
-
-namespace RxBlazorV2Sample.Pages
-{
-    public partial class Counter1 : ObservableComponent<CounterModel>
-    {
-    }
-}
